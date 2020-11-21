6f10a97ddacbbe80e5b2189ef0259f4b67fbf839
Aakash Tyagi <aakash.tyg93@gmail.com>
2020-11-21 20:36:03 +0530

    Create basic server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 8000;
+const app = express();
+
+app.use('/', require('./routes'));
+
+app.listen(PORT, function (error) {
+  if (error) console.log('error : ', error);
+
+  console.log('Server is runing!');
+});
